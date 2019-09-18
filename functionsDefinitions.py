2bc59c796e329315edc02c00c06b4d18d42f5f4b
chipdelmal <chipdelmal@gmail.com>
2019-09-18 10:27:22 -0700

    Added functions to cleanMerge and getOverlap between dataframes

--- functionsDefinitions.py
+++ functionsDefinitions.py
@@ -1,5 +1,25 @@
+import pandas as pd
+
+
 def getSortedCols(dataframe):
     return sorted(list(dataframe.columns))
 
+
 def listColEls(dataframe, column):
     return list(dataframe.get(column))
+
+
+def getOverlappingColumns(dfA, dfB):
+    dataFrames = [dfA, dfB]
+    cols = [set(frame.columns) for frame in dataFrames]
+    overlappingColumns = list(set.intersection(*cols))
+    return overlappingColumns
+
+
+def cleanMergeDataframes(dfLeft, dfRight, how='inner'):
+    overlap = getOverlappingColumns(dfLeft, dfRight)
+    dataMerge = pd.merge(
+        left=dfLeft, right=dfRight,
+        on=overlap, how='inner'
+    )
+    return dataMerge
