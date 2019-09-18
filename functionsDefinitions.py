72842ebbb9903f36b9fa65594d4351be8eb7b616
chipdelmal <chipdelmal@gmail.com>
2019-09-18 10:46:10 -0700

    Added comments to the functions definitions

--- functionsDefinitions.py
+++ functionsDefinitions.py
@@ -2,14 +2,17 @@ import pandas as pd
 
 
 def getSortedColumns(dataframe):
+    # Returns a list of the dataframe's columns in alphanumerical sorting
     return sorted(list(dataframe.columns))
 
 
 def listColumnElements(dataframe, column):
+    # Returns a list with the elements contained in the dataframe's column
     return list(dataframe.get(column))
 
 
 def getOverlappingColumns(dfA, dfB):
+    # Returns a list with the columns that overlap between dataframes
     dataFrames = [dfA, dfB]
     cols = [set(frame.columns) for frame in dataFrames]
     overlappingColumns = list(set.intersection(*cols))
@@ -17,6 +20,7 @@ def getOverlappingColumns(dfA, dfB):
 
 
 def cleanMergeDataframes(dfLeft, dfRight, how='inner'):
+    # Merges two dataframes without repeated columns
     overlap = getOverlappingColumns(dfLeft, dfRight)
     dataMerge = pd.merge(
         left=dfLeft, right=dfRight,
